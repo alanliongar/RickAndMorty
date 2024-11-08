b7d7c55c90e3a116857dbbf50faa22d5eda9caad
alanliongar <alan.lucindo.gomes@alumni.usp.br>
2024-11-08 20:04:44 -0300

    Starting the project

--- app/src/main/java/com/devspace/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/devspace/rickandmorty/MainActivity.kt
@@ -63,7 +63,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CharacterListScreen()
+                    RickandMortyApp()
                 }
             }
         }
