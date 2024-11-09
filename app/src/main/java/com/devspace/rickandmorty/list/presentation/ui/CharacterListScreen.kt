a9d22cc09405f0b80d5192c8a6614bd2f1b262e8
alanliongar <alan.lucindo.gomes@alumni.usp.br>
2024-11-09 20:45:16 -0300

    Inclusão de testes. 17 horas até agora.

--- app/src/main/java/com/devspace/rickandmorty/list/presentation/ui/CharacterListScreen.kt
+++ app/src/main/java/com/devspace/rickandmorty/list/presentation/ui/CharacterListScreen.kt
@@ -25,7 +25,6 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
--- app/src/test/java/com/devspace/rickandmorty/list/CharacterListRepositoryTest.kt
+++ app/src/test/java/com/devspace/rickandmorty/list/CharacterListRepositoryTest.kt
@@ -1,9 +1,14 @@
 package com.devspace.rickandmorty.list
 
+import android.accounts.NetworkErrorException
+import com.devspace.rickandmorty.common.model.Character
 import com.devspace.rickandmorty.list.data.CharacterListRepository
 import com.nhaarman.mockitokotlin2.mock
 import com.devspace.rickandmorty.list.data.local.CharacterListLocalDataSource
 import com.devspace.rickandmorty.list.data.remote.CharacterListRemoteDataSource
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
 
 class CharacterListRepositoryTest {
     private val local: CharacterListLocalDataSource = mock()
@@ -11,5 +16,28 @@ class CharacterListRepositoryTest {
 
     private val underTest by lazy {
         CharacterListRepository(local = local, remote = remote)
+    }
+
+
+    @Test
+    fun `Given no internet connection When getting character list Then return local data`() {
+        runTest {
+            //Given
+            val localList = listOf(
+                Character(
+                    id = 1,
+                    name = "Rick Sanchez",
+                    image = "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+                    specie = "Human",
+                    isFavorite = false
+                )
+            )
+            whenever(remote.getFilteredCharacters()).thenReturn(Result.failure(NetworkErrorException()))
+            whenever(local.getCharacterList()).thenReturn(localList)
+
+            //Then
+
+            //When
+        }
     }
 }
