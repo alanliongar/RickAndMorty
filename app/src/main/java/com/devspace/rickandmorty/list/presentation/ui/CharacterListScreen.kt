9433316ac163761a1ed2631099ac6527958db038
alanliongar <alan.lucindo.gomes@alumni.usp.br>
2024-11-09 00:31:13 -0300

    Segunda commit com mvvm

--- app/src/main/java/com/devspace/rickandmorty/list/presentation/ui/CharacterListScreen.kt
+++ app/src/main/java/com/devspace/rickandmorty/list/presentation/ui/CharacterListScreen.kt
@@ -3,11 +3,14 @@ package com.devspace.rickandmorty.list.presentation.ui
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
+import android.os.Build.VERSION.SDK_INT
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -15,10 +18,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -34,18 +39,23 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.palette.graphics.Palette
 import coil.ImageLoader
 import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
 import coil.request.ImageRequest
 import coil.request.SuccessResult
-import com.devspace.rickandmorty.list.model.CharacterDto
-import com.devspace.rickandmorty.list.model.CharacterListResponse
+import coil.size.Size
+import com.devspace.rickandmorty.R
 import com.devspace.rickandmorty.list.presentation.CharacterListViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -62,9 +72,12 @@ fun CharacterListScreen(
 }
 
 @Composable
-private fun CharacterListContent(listOfCharacters: CharacterListUiState, onClick: (CharacterUiData) -> Unit) {
-    if (listOfCharacters == null) {
-        Text("Carregando....")
+private fun CharacterListContent(
+    listOfCharacters: CharacterListUiState,
+    onClick: (CharacterUiData) -> Unit
+) {
+    if (listOfCharacters.isLoading) {
+        Text("Carregando....") //teladecarregando
     } else {
         CharactersGrid(listOfCharacters, onClick = onClick)
     }
@@ -82,9 +95,10 @@ private fun CharactersGrid(
         contentPadding = PaddingValues(0.dp),
         verticalArrangement = Arrangement.spacedBy(3.dp),
         horizontalArrangement = Arrangement.spacedBy(3.dp)
-    ) { items(listOfCharacters.characters.size) { index ->
-                CharacterCard(character = listOfCharacters.characters[index], onClick = onClick)
-            }
+    ) {
+        items(listOfCharacters.characters.size) { index ->
+            CharacterCard(character = listOfCharacters.characters[index], onClick = onClick)
+        }
     }
 }
 
@@ -92,10 +106,13 @@ private fun CharactersGrid(
 private fun CharacterCard(character: CharacterUiData, onClick: (CharacterUiData) -> Unit) {
     val imageUrl: String = character.image
     val context = LocalContext.current
+    var isLoading by remember { mutableStateOf(true) }
     var color by remember { mutableStateOf(Color.Transparent) }
     LaunchedEffect(imageUrl) {
+        isLoading = true
         val dominantColor = getDominantColorFromImage(context, imageUrl)
         color = dominantColor ?: Color.Transparent
+        isLoading = false
     }
     Column(
         modifier = Modifier
@@ -109,25 +126,112 @@ private fun CharacterCard(character: CharacterUiData, onClick: (CharacterUiData)
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        AsyncImage(
-            model = ImageRequest.Builder(LocalContext.current)
-                .data(imageUrl)
-                .build(),
-            contentDescription = "Image of ${character.name}",
-            modifier = Modifier
-                .width(150.dp)
-                .height(150.dp)
-                .clip(RoundedCornerShape(16.dp)),
-            contentScale = ContentScale.Fit
-        )
-        Spacer(modifier = Modifier.height(8.dp))
+        if (isLoading) {
+            CharacterListIsLoading()
+        } else {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(imageUrl)
+                    .build(),
+                contentDescription = "Image of ${character.name}",
+                modifier = Modifier
+                    .width(150.dp)
+                    .height(150.dp)
+                    .clip(RoundedCornerShape(16.dp)),
+                contentScale = ContentScale.Fit
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = character.name,
+                fontWeight = FontWeight.Bold,
+                color = readableColor(color),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth(),
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+}
+
+@Composable
+fun CharacterListIsLoading() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            GifImage(
+                modifier = Modifier.size(500.dp).clip(RoundedCornerShape(250.dp)) // Tamanho do GIF
+            )
+            Text("Loading...", fontSize = 24.sp)
+        }
+    }
+}
+
+@Composable
+fun GifImage(
+    modifier: Modifier = Modifier,
+) {
+    val context = LocalContext.current
+    val imageLoader = ImageLoader.Builder(context)
+        .components {
+            if (SDK_INT >= 30) {
+                add(ImageDecoderDecoder.Factory())
+            } else {
+                add(GifDecoder.Factory())
+            }
+        }
+        .build()
+    Image(
+        painter = rememberAsyncImagePainter(
+            ImageRequest.Builder(context).data(data = R.drawable.loading_image).apply {
+                size(Size.ORIGINAL)
+            }.build(),
+            imageLoader = imageLoader
+        ),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+
+@Composable
+fun CharacterListErrorUiState(errorMsg: String? = null) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
-            text = character.name,
-            fontWeight = FontWeight.Bold,
-            color = readableColor(color),
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth(),
-            overflow = TextOverflow.Ellipsis
+            fontSize = 36.sp,
+            modifier = Modifier.padding(16.dp),
+            color = Color.Red,
+            text = "Ohh No!",
+            fontWeight = FontWeight.Bold
+        )
+        Image(
+            painter = painterResource(id = R.drawable.error_image),
+            contentDescription = "Imagem de erro",
+            modifier = Modifier
+                .width(432.dp)
+                .height(577.dp),
+            contentScale = ContentScale.FillHeight
+        )
+        Text(
+            fontSize = 32.sp,
+            modifier = Modifier.padding(16.dp),
+            color = Color.Red,
+            text = "Something went wrong!",
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            fontSize = 20.sp,
+            modifier = Modifier.padding(16.dp),
+            color = Color.Blue,
+            text = errorMsg ?: "Go back and try again",
+            fontWeight = FontWeight.Bold
         )
     }
 }
